483a94c37bf327e2d89f8421fb4a39a9672c3315
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2021-08-10 15:38:54 +0530

    Add files via upload

---
+++ SpringBootRegistrationLogin/SpringBootRegistrationLogin/src/main/java/net/codejava/SpringBootRegistrationLogin.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegistrationLogin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRegistrationLogin.class, args);
+	}
+
+}
